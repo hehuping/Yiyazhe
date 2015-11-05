0e53ebddf97e0ee5d5795cf3ab5b2ea2f103e12d
hehuping <595549109@qq.com>
2015-11-05 16:24:52 +0800

    搜索接口

--- yiyazhe/Home/Controller/SearchController.class.php
+++ yiyazhe/Home/Controller/SearchController.class.php
@@ -24,4 +24,29 @@ class SearchController extends Controller {
 		$this->display();
 		
 	}
+	
+	public function sendSearch(){
+		
+		$p = I('p');
+		empty($p) ? $p=1 : $p;
+		$obj = new Data();
+		$keywords = I('keywords');
+		if(empty($keywords)){
+			$obj->status = -1;
+			$obj->error = "关键字不能为空";
+			$this->ajaxReturn($obj);
+			die;
+		}
+		$model = D('Search');
+		list($result, $show, $count)=$model->getSearchGoods($p-1, $keywords, 80);
+
+		$obj->count = $count;
+		$obj->data = $result;
+		$obj->page = $p;
+		$obj->barnner = array(
+				//'http://www.yiyazhe.com/Public/images/index/cateboys.png',
+		);
+		 
+		$this->ajaxReturn($obj);
+	}
 }
