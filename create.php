43886cfc73d77f3bd40dea7dcee8c63d11fa8c9c
hehuping <595549109@qq.com>
2016-05-24 15:58:12 +0800

    accs

--- create.php
+++ create.php
@@ -37,7 +37,8 @@ function post($url, $post_data = '', $timeout = 5){//curl
 function getaccss(){
     $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxb096e505f9556191&secret=d4624c36b6795d1d99dcf0547af5443d";
     $content = file_get_contents($url);
-    return $content;
+
+    return json_deencode($content);
 }
 
 echo getaccss();
