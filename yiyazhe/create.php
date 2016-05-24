fa938368e6caf501d80a9221c8736d0123eb1844
hehuping <595549109@qq.com>
2016-05-24 15:52:02 +0800

    微信acc

---
+++ yiyazhe/create.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/5/24
+ * Time: 15:38
+ */
+
+function post($url, $post_data = '', $timeout = 5){//curl
+
+    $ch = curl_init();
+
+    curl_setopt ($ch, CURLOPT_URL, $url);
+
+    curl_setopt ($ch, CURLOPT_POST, 1);
+
+    if($post_data != ''){
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+
+    }
+
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+    curl_setopt($ch, CURLOPT_HEADER, false);
+
+    $file_contents = curl_exec($ch);
+
+    curl_close($ch);
+
+    return $file_contents;
+
+}
+
+function getaccss(){
+    $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxb096e505f9556191&secret=d4624c36b6795d1d99dcf0547af5443d";
+    $content = file_get_contents($url);
+    return $content;
+}
+
+echo getaccss();
+
+
+
+$data = array(
+
+);
+
+$mune = '{
+     "button":[
+     {
+          "type":"click",
+          "name":"今日头条",
+          "key":"V1001_TODAY_MUSIC"
+      },
+      {
+           "name":"哈哈",
+           "sub_button":[
+           {
+               "type":"view",
+               "name":"搜索",
+               "url":"http://www.soso.com/"
+            },
+            {
+               "type":"view",
+               "name":"视频",
+               "url":"http://v.qq.com/"
+            },
+            {
+               "type":"click",
+               "name":"赞一下我们",
+               "key":"V1001_GOOD"
+            }]
+       }]
+ }';
+
+
+
