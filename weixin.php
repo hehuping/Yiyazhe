c378b69aada0f46332a0250fe28a5c98d747d288
hehuping <595549109@qq.com>
2016-05-24 22:30:15 +0800

    微信acc

--- weixin.php
+++ weixin.php
@@ -21,7 +21,42 @@ class wechatCallbackapiTest
         }
     }
 
-    public function responseMsg()
+	public function contentImage($fromUsername, $toUsername, $picUrl, $link, $title, $content){
+		$newsTplHead = "<xml>
+						<ToUserName><![CDATA[$fromUsername]]></ToUserName>
+						<FromUserName><![CDATA[$toUsername]]></FromUserName>
+						<CreateTime>time()</CreateTime>
+						<MsgType><![CDATA[news]]></MsgType>
+						<ArticleCount>1</ArticleCount>
+						<Articles>";
+		$newsTplBody = "<item>
+						<Title><![CDATA[$title]]></Title>
+						<Description><![CDATA[$content]]></Description>
+						<PicUrl><![CDATA[$picUrl]]></PicUrl>
+						<Url><![CDATA[$link]]></Url>
+						</item>";
+		$newsTplFoot = "</Articles>
+						<FuncFlag>0</FuncFlag>
+						</xml>";
+		return $newsTplHead.$newsTplBody.$newsTplFoot;
+	}
+
+	public function contentText($fromUsername, $toUsername, $content){
+		$textTpl = "<xml>
+					<ToUserName><![CDATA[$fromUsername]]></ToUserName>
+					<FromUserName><![CDATA[$toUsername]]></FromUserName>
+					<CreateTime>time()</CreateTime>
+					<MsgType><![CDATA[text]]></MsgType>
+					<Content><![CDATA[$content]]></Content>
+					<FuncFlag>0</FuncFlag>
+					</xml>";
+		return $textTpl;
+	}
+
+	/**
+	 *
+     */
+	public function responseMsg()
     {
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
@@ -39,6 +74,7 @@ class wechatCallbackapiTest
 				$PicUrl = $postObj->PicUrl;
 				$MediaId = $postObj->MediaId;
 				$MsgId = $postObj->MsgId;
+				$Event =  $postObj->Event;
 			/**/
                 $keyword = trim($postObj->Content);
                 $time = time();
@@ -59,42 +95,15 @@ class wechatCallbackapiTest
 
                 }elseif($MsgType == "image"){
 					$Url = 'http://s2.juancdn.com/bao/160523/7/4/57429a03151ad16a468b45c7_400x400.jpg';
-					$newsTplHead = "<xml>
-									<ToUserName><![CDATA[$fromUsername]]></ToUserName>
-									<FromUserName><![CDATA[$toUsername]]></FromUserName>
-									<CreateTime>%$time</CreateTime>
-									<MsgType><![CDATA[news]]></MsgType>
-									<ArticleCount>1</ArticleCount>
-									<Articles>";
-					$newsTplBody = "<item>
-									<Title><![CDATA[测试]]></Title>
-									<Description><![CDATA[哈哈哈哈哈哈]]></Description>
-									<PicUrl><![CDATA[$Url]]></PicUrl>
-									<Url><![CDATA[http://www.qq.com]]></Url>
-									</item>";
-					$newsTplFoot = "</Articles>
-									<FuncFlag>0</FuncFlag>
-									</xml>";
-                	echo $newsTplHead.$newsTplBody.$newsTplFoot;
-                }elseif($MsgType == "event"){
-					$Url = 'http://s2.juancdn.com/bao/160523/7/4/57429a03151ad16a468b45c7_400x400.jpg';
-					$newsTplHead = "<xml>
-									<ToUserName><![CDATA[$fromUsername]]></ToUserName>
-									<FromUserName><![CDATA[$toUsername]]></FromUserName>
-									<CreateTime>%$time</CreateTime>
-									<MsgType><![CDATA[news]]></MsgType>
-									<ArticleCount>1</ArticleCount>
-									<Articles>";
-					$newsTplBody = "<item>
-									<Title><![CDATA[感谢关注]]></Title>
-									<Description><![CDATA[欢迎您关注我们，我们会给你带来不一样的乐趣]]></Description>
-									<PicUrl><![CDATA[$Url]]></PicUrl>
-									<Url><![CDATA[http://www.qq.com]]></Url>
-									</item>";
-					$newsTplFoot = "</Articles>
-									<FuncFlag>0</FuncFlag>
-									</xml>";
-					echo $newsTplHead.$newsTplBody.$newsTplFoot;
+					echo $this->contentImage($fromUsername, $toUsername, $Url, "http://www.youku.com","这是个图片消息","图片消息响应成功");
+
+                }elseif($MsgType == "event" && $Event == "CLICK"){
+					$Url = 'http://s2.juancdn.com/bao/160524/8/0/57442079151ad105148b45b3_400x400.jpg';
+					echo $this->contentImage($fromUsername, $toUsername, $Url, "http://www.qq.com","点击消息","点击按钮得到的消息");
+
+				}elseif($MsgType == "event" && $Event == "subscribe"){
+					$Url = 'http://s2.juancdn.com/bao/160523/6/f/5742771e151ad1516a8b45c7_400x400.jpg';
+					echo $this->contentImage($fromUsername, $toUsername, $Url, "http://www.qq.com","欢迎你小伙伴","感谢您关注我们，有了我们世界会更有趣");
 				}
 
         }else {
