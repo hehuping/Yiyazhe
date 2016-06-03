9b4744d53c629254aea2fbfdd8646d670c9992de
hehuping <595549109@qq.com>
2016-06-04 00:09:08 +0800

    微信acc

--- yiyazhe/Home/Controller/FaceController.class.php
+++ yiyazhe/Home/Controller/FaceController.class.php
@@ -133,4 +133,124 @@ class FaceController extends Controller
         $this->display();
     }
 
+    public function similler(){
+        layout(false);
+
+       // import("Org.Net.Faceset");
+       // import("Org.Net.Facepp");
+
+        /*$wuser = M('wuser');
+
+        $info = getUserInfo();
+        $has = $wuser->where("openid='{$info->openid}'")->find();
+        if(!$has){
+            $data = array(
+                'openid' => $info->openid,
+                'nickname' => $info->nickname,
+                'sex' => $info->sex,
+                'province' => $info->province,
+                'city' => $info->city,
+                'country' => $info->country,
+                'headimgurl' => $info->headimgurl,
+                'reg_time' => date("Y-m-d H:i:s",time()),
+            );
+            $wuser->add($data);
+        }
+
+
+        $this->assign('info',$info);*/
+        $this->display();
+    }
+
+    public function async(){
+        $respons = array('s' => 0);
+        import("Org.Net.Faceset");
+        import("Org.Net.Facepp");
+        $facepp = new \Facepp();
+        $faceset = new \Faceset('hehe');
+        $data = I("img");
+        $arr = array(
+            'data:image/jpg;base64,',
+            'data:image/png;base64,',
+            'data:image/jpeg;base64,',
+        );
+        $rand = 'abcdefg';
+        $file_name = time().str_shuffle($rand);
+        $data = str_replace($arr,'',$data);
+        $img = base64_decode($data);
+        $handle = file_put_contents("./wpic2/".$file_name.".jpg",$img);
+        $size = filesize("./wpic2/".$file_name.".jpg");
+
+        if($size > 1024*1024*5){
+            $respons['s'] = 2;
+            $respons['error'] = "图片太大了，不能超过5M";
+            echo json_encode($respons);
+            die;
+        }
+
+        if($size > 1024*1024){
+            $image = new \Think\Image();
+            $image->open("./wpic2/".$file_name.".jpg");
+            $image->thumb(800, 800,\Think\Image::IMAGE_THUMB_FILLED)->save("./wpic2/".$file_name.".jpg");
+        }
+
+        $imageins = M('image');
+
+        if($handle){
+            $params['img']          = $_SERVER['DOCUMENT_ROOT']."/wpic2/$file_name.jpg";
+            $params['attribute']    = 'gender,age,race,smiling';
+            $rep = $facepp->execute('/detection/detect',$params);
+
+            if(empty($rep['body'])){
+                $respons['s'] = 1;
+                $respons['error'] = "图像无法识别";
+
+                echo json_encode($respons);
+                die;
+            }
+
+            $rs = json_decode($rep['body']);
+            foreach($rs->face as $k=>$v){
+                $data = array(
+                    'face_id' => $v->face_id,
+                    'img_id' => $rs->img_id,
+                    //'url' => $rs->url,
+                    'pic_name' => $file_name.".jpg",
+                );
+                //写入数据库
+                $imageins->add($data);
+
+                $fres = $faceset->search($v->face_id,1);
+
+            }
+        }
+        $fres = json_decode($fres['body']);
+        //print_r($fres->candidate);
+        $fid = $fres->candidate[0]->face_id;
+        $similarity = $fres->candidate[0]->similarity;
+
+        $data = $imageins->where("face_id='{$fid}'")->find();
+        $respons['img'] = $data['pic_name'];
+        $respons['similarity']=$similarity;
+
+        echo json_encode($respons);
+        // $facepp->execute('',array());
+        //echo json_encode($rep);
+    }
+
+    public function train(){
+        import("Org.Net.Faceset");
+        import("Org.Net.Facepp");
+
+        $faceset = new \Faceset('hehe');
+        print_r($faceset->trainSearch());
+    }
+
+    public function getsession(){
+        import("Org.Net.Faceset");
+        import("Org.Net.Facepp");
+        $faceset = new \Faceset('hehe');
+        print_r($faceset->getSession('f50abe8926834988838e4d118d5f54e7'));
+    }
+
 }
